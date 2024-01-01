c6de1a5d0b5d3992939c5f367ffb53c2681a9b13
tomasilva03 <tomas.reb.silva@gmail.com>
2024-01-01 13:05:51 +0000

    fix

--- client/src/components/MainLayout.jsx
+++ client/src/components/MainLayout.jsx
@@ -12,11 +12,11 @@ const MainLayout = ({ children }) => {
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="mr-auto">
                             <Nav.Link href="/">Home</Nav.Link>
-                            <Nav.Link href="/login">Login</Nav.Link>
-                            <Nav.Link href="/register">Register</Nav.Link>
                             {/* Add other navbar links as needed */}
                         </Nav>
                         <Nav>
+                            <Nav.Link href="/login">Login</Nav.Link>
+                            <Nav.Link href="/register">Register</Nav.Link>
                             {/* Add user-related actions like logout */}
                         </Nav>
                     </Navbar.Collapse>

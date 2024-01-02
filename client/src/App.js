ceaa1d23c3cf816a14ea10d1893af6987a1697f5
tomasilva03 <tomas.reb.silva@gmail.com>
2024-01-02 17:29:48 +0000

    trying to still fix

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ import HomePage from './pages/homePage';
 
 const App = () => {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route exact path="/register" element={<RegisterPage/>} />
         <Route exact path="/login" element={<LoginPage/>} />
